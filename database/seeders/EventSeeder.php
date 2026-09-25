c67e6f42ec4314b64121c6f56a2715493513aae5
Ruhiology Institute <dev@ruhiologyinstitute.com>
2026-09-25 13:26:28 +0700

    Add SMANTT event default seed to EventSeeder

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -42,5 +42,25 @@ class EventSeeder extends Seeder
                 'description' => 'Kegiatan pengukuran resmi tingkat kesadaran batin & vitalitas emosional peserta pelatihan.'
             ]
         );
+
+        Event::firstOrCreate(
+            ['event_code' => 'SMANTT'],
+            [
+                'title' => 'Ruhiologi Asesment',
+                'access_type' => 'EVENT_PROGRAM',
+                'institution_name' => 'SMAN Titian Teras Jambi',
+                'program_id' => $program->id ?? null,
+                'instrument_id' => $instrument->id ?? null,
+                'start_date' => now(),
+                'end_date' => now()->addMonths(6),
+                'status' => 'active',
+                'target_category' => 'Pelajar',
+                'province_id' => 5,
+                'regency_id' => 10,
+                'group_label' => 'Pilih Kelas',
+                'custom_subcategories' => ['X-A', 'X-B', 'X-C', 'X-D', 'X-E', 'X-F'],
+                'description' => 'Asesmen Ruhiologi SMAN Titian Teras Jambi.'
+            ]
+        );
     }
 }
